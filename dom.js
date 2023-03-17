388b3d4ff64a12d4b7f15fc210fbe6283bc3b53a
Sahilnarang310 <softwaredev310@gamil.com>
2023-03-18 00:08:50 +0530

    response of task 6

--- dom.js
+++ dom.js
@@ -16,12 +16,43 @@
 //   listElements[i].style.color = "green"; // Change to any color you want
 //}
 //  get element by tag name as same as the class works
-var li=document.getElementsByTagName('li');
-console.log(li);
-li[2].textContent="hello2";
-console.log(li[2]);
-li[2].style.backgroundColor='green';
-for (let i = 0; i < li.length; i++)
- {
-  li[i].style.fontWeight = "bold";
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// li[2].textContent="hello2";
+// console.log(li[2]);
+// li[2].style.backgroundColor='green';
+// for (let i = 0; i < li.length; i++)
+//  {
+//   li[i].style.fontWeight = "bold";
+// }
+//Qwery Selector it only grab first one!
+// var header =document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+//  var input=document.querySelector('input');
+//  input.value='Hello World';
+
+//  var submit=document.querySelector('input[type="submit"]');
+//  submit.value="SEND";
+ 
+//  var item =document.querySelector('.list-group-item');
+//  item.style.color="red";
+
+//  var lastitem =document.querySelector('.list-group-item:last-child');
+//  lastitem.style.color="blue";
+
+ var seconditem =document.querySelector('.list-group-item:nth-child(2)');
+ seconditem.style.color="green"; 
+//  seconditem.style.backgroundColor="green"; 
+//  Deliverable its messed up because if make seccond text color and background color green then text will invisible!
+
+//  Qwery SelectorAll// works similar as tagname ans class name;
+var titles =document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='Hello';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+// var even =document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++){
+  odd[i].style.backgroundColor='green';
+  // even[i].style.backgroundColor='#ccc';
 }
