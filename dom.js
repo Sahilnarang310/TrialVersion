8eb25d83a63a1f9e7811f8c732b19e7a50c4f78d
Sahilnarang310 <softwaredev310@gamil.com>
2023-03-17 22:51:34 +0530

    Response of task 5 DOM

--- dom.js
+++ dom.js
@@ -1,18 +1,27 @@
 // Get Element By Class Name
-var items=document.getElementsByClassName('list-group-item');
-console.log(items)
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items)
 // items[2].textContent="hello2";
-console.log(items[2]);
+// console.log(items[2]);
 
 // items.style.fontWeight='bold';
-items[2].style.backgroundColor='green';
+// items[2].style.backgroundColor='green';
 // items[2].style.background-color:"green";
 // Get all the list elements
-const listElements = document.getElementsByTagName("li");
+// const listElements = document.getElementsByTagName("li");
 
 // Loop through the list elements and set the CSS styles
-for (let i = 0; i < listElements.length; i++) {
-  listElements[i].style.fontWeight = "bold";
+// for (let i = 0; i < listElements.length; i++) {
+//   listElements[i].style.fontWeight = "bold";
 //   listElements[i].style.color = "green"; // Change to any color you want
+//}
+//  get element by tag name as same as the class works
+var li=document.getElementsByTagName('li');
+console.log(li);
+li[2].textContent="hello2";
+console.log(li[2]);
+li[2].style.backgroundColor='green';
+for (let i = 0; i < li.length; i++)
+ {
+  li[i].style.fontWeight = "bold";
 }
-
