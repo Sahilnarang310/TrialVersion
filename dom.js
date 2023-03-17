334794b297c18b88fdeb7721fe2cd005f2e3c169
Sahilnarang310 <softwaredev310@gamil.com>
2023-03-17 20:26:08 +0530

    answer of task 4

--- dom.js
+++ dom.js
@@ -1,31 +1,18 @@
-// console.log(document.URL);
-// // console.log()
-// console.log(document.head);
-// console.log(document.title);
-// // document.title=123;
-// // console.log(document.title);
-// console.log(document.head,document.doctype,document.domain);
-// console.log(document.all[10]);
-// document.all[10].textContent="Hello";
-// console.log(document.forms);
-// // console.log(document.links);
-// console.log(document.images);
-// console.log(document.getElementById('header-title'));
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent="Hello";
-//headerTitle.innerText='God help ' // we over write the hello
-// console.log(headerTitle.textContent);
-// console.log(headerTitle.innerText); // it show the styling also! main difference between both.
-// headerTitle.innerHTML='<h3>Hello</h3>'
-headerTitle.style.borderBottom='solid 3px #000';
-header.style.borderBottom='solid 3px #000';
-// console.log(document.div);
-const titleElement = document.querySelector('h2.title');
-titleElement.style.fontWeight = "bold";
-titleElement.style.color = "green";
+// Get Element By Class Name
+var items=document.getElementsByClassName('list-group-item');
+console.log(items)
+// items[2].textContent="hello2";
+console.log(items[2]);
 
+// items.style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+// items[2].style.background-color:"green";
+// Get all the list elements
+const listElements = document.getElementsByTagName("li");
 
-
+// Loop through the list elements and set the CSS styles
+for (let i = 0; i < listElements.length; i++) {
+  listElements[i].style.fontWeight = "bold";
+//   listElements[i].style.color = "green"; // Change to any color you want
+}
 
